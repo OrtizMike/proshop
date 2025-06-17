a2c94ba519ef39830494736e8cee1a77b63ccc64
Miguel Ortiz <m.ortiz.11@gmail.com>
2025-06-17 16:58:41 -0600

    Start with FrontEnd

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,24 @@
 import React from 'react'
+import { Routes, Route } from "react-router-dom";
+import { Container } from 'react-bootstrap'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import HomeScreen from './screens/HomeScreen'
+import ProductScreen from './screens/ProductScreen'
 
 const App = props => {
   return (
     <>
-      <h1>Welcome to Orvez</h1>
+      <Header />
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route exact path="/" element={<HomeScreen />}></Route>
+            <Route exact path="/product/:id" element={<ProductScreen />}></Route>
+          </Routes>
+        </Container>
+      </main>
+      <Footer />
     </>
   )
 }
